b869fbba4aa87bc2f30de7839103f74b01e8fb34
Risa Fujii <risaf1231@gmail.com>
2019-03-29 16:32:52 +0900

    Add resize function; next need to combine with upload method

--- handler.js
+++ handler.js
@@ -1,5 +1,5 @@
 const AWS = require('aws-sdk');
-const IM = require('imagemagick');
+const im = require('imagemagick');
 const fs = require('fs');
 // const path = require('path');
 
@@ -9,7 +9,17 @@ const s3 = new AWS.S3({
   region: 'ap-northeast-1'
 });
 
-const uploadFile = './boggle.jpg'
+im.resize({
+  srcPath: 'boggle.jpg',
+  dstPath: 'boggle_resized.jpg',
+  width: 120,
+  height: 120
+}, function(err, stdout, stderr){
+     if (err) throw err;
+     console.log('Resized the image successfully');
+});
+
+const uploadFile = './boggle_resized.jpg'
 
 const params = {
   Bucket: 'source-image-bois',
